ffe26e6ca8759084e5d758fa912007c57a03a212
froggal <doeon111@naver.com>
2023-06-04 21:18:37 +0900

    solved 9498 with python3

---
+++ Python/9498.py
@@ -0,0 +1,12 @@
+data = int(input())
+
+if data >= 90 and data <= 100 :
+    print('A')
+elif data >= 80 and data <= 89 :
+    print('B')
+elif data >= 70 and data <= 79 :
+    print('C')
+elif data >= 60 and data <= 69 :
+    print('D')
+else :
+    print('F')

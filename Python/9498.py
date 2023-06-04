9b0059b741209005d471e58f1046d35c46c950ff
froggal <doeon111@naver.com>
2023-06-04 21:22:07 +0900

    edit 9498

--- Python/9498.py
+++ Python/9498.py
@@ -1,12 +1,12 @@
 data = int(input())
 
-if data >= 90 and data <= 100 :
+if data >= 90 :
     print('A')
-elif data >= 80 and data <= 89 :
+elif data >= 80 :
     print('B')
-elif data >= 70 and data <= 79 :
+elif data >= 70 :
     print('C')
-elif data >= 60 and data <= 69 :
+elif data >= 60 :
     print('D')
 else :
     print('F')
